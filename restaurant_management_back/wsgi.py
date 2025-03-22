44a2698b762c1a8c9b7ca4b2ba4d1e02afc47411
hexaalif <shadullahsad9@gmail.com>
2025-03-22 17:35:41 +0600

    chng in wesgi

--- restaurant_management_back/wsgi.py
+++ restaurant_management_back/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management_back.settings')
 
 application = get_wsgi_application()
+app=application
